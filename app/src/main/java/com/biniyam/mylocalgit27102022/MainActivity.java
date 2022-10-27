458b1cb7a74b4be28b3417e5fd526d16fc727653
bygxs <114972065+bygxs@users.noreply.github.com>
2022-10-27 12:16:16 +0200

    12:33 Made some style changes background and textStyle

---
+++ app/src/main/java/com/biniyam/mylocalgit27102022/MainActivity.java
@@ -0,0 +1,22 @@
+package com.biniyam.mylocalgit27102022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**
+         * // DONE: 11:59 Just now I fixed my Git version control.
+         * I tested the path in the main start project first dialogue window
+         * now I am going to make commits into my local Git path
+         * I want no GitHub shit interference
+         */
+
+
+    }
+}
